02e5352aeab06659d8cd883ffcd04f1cd53701cf
Michael Belikov <thebestlucktravel@gmail.com>
2026-03-16 15:36:30 +0200

    telegram debug

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -1,8 +1,46 @@
+import { NextResponse } from "next/server"
+
+const TOKEN = "8765331231:AAGjRTI1zgvAuPaOq5tCJNpjQy21GAvCcCM"
+const CHAT_ID = -1003518722365
+
 export async function POST(req: Request) {
 
-  const data = await req.json()
+  try {
 
-  console.log("TELEGRAM UPDATE:", data)
+    const { name, phone, service } = await req.json()
 
-  return new Response("ok")
+    const message = `
+Новая заявка с сайта
+
+Имя: ${name}
+Телефон: ${phone}
+Услуга: ${service}
+`
+
+    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: message
+      })
+    })
+
+    return NextResponse.json({ success: true })
+
+  } catch (error) {
+
+    console.error("Telegram error:", error)
+
+    return NextResponse.json(
+      { success: false },
+      { status: 500 }
+    )
+
+  }
+
 }
