8a905dc2149da6663d3c64c87d819089dcc24353
Michael Belikov <thebestlucktravel@gmail.com>
2026-03-20 20:22:02 +0200

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
-export const metadata = {
-  verification: {
-    google: "MuQ4d2r4RyBZZqpz..."
-  }
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="uk">
+      <body>{children}</body>
+    </html>
+  )
 }
