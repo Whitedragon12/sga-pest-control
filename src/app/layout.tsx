af5cf817a9dc051bc21d1521cf2ed76b35ad0867
Michael Belikov <thebestlucktravel@gmail.com>
2026-03-20 21:48:14 +0200

    fix: globals path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import "../globals.css"
+import "./globals.css"
 
 
 const geistSans = Geist({
